7b781542beb4b4a3280aaf60a8e4073d63ee1ea6
Jason Koo <jalakoo@gmail.com>
2021-05-25 16:57:42 -0700

    Sendbird init moved to contentView

--- sendbirduikit_swiftui/ContentView.swift
+++ sendbirduikit_swiftui/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 import SendBirdUIKit
 
 struct ContentView: View {
+    init() {
+        
+        // Initialize the SendbirdSDK with your application id,
+        // also found in the Sendbird dashboard
+        SBUMain.initialize(applicationId:"YOUR_SENDBIRD_APPLICATION_ID")
+        
+        // After your user has authenticated, use a string
+        // identifier unique to that user to pass to Sendbird.
+        SBUGlobals.CurrentUser = SBUUser(userId: "A_SENDBIRD_USER_ID")
+
+        // Connect with Sendbird now that the CurrentUser is set
+        SBUMain.connect { (user, error) in
+            
+            // user object will be an instance of SBDUser
+            guard let _ = user else {
+                print("ContentView: init: connect: ERROR: \(String(describing: error))")
+                return
+            }
+        }
+    }
+    
     var body: some View {
-        ChannelListContainer()
+        ChannelListViewContainer()
     }
 }
 
